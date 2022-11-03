9aafa1b78c6929ffd627228e7d1470dc5bbc0e28
Dan Palmer <dj.palmer81@gmail.com>
2022-11-03 15:50:17 +0200

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 10:53:46 by dpalmer           #+#    #+#             */
+/*   Updated: 2022/11/03 12:16:01 by dpalmer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 12:17:08 by dpalmer           #+#    #+#             */
+/*   Updated: 2022/11/03 12:19:54 by dpalmer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/* Misc utilites needed for evaluation. These will eventually be subsumed	**
+** into the general LIBFT file(s) once this project is complete. 			**/
+
